35c51672e999b26288ea0fd4d124645977bebf9c
ibrahim-hanif <guptavasudev37@gmail.com>
2026-05-12 11:48:10 +0200

    todo: run new ref TLP files

--- examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/raft_learn.py
+++ examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/raft_learn.py
@@ -6,13 +6,16 @@ import os
 #%%
 # File management
 dir_this = os.path.dirname( os.path.abspath(__file__) )
+dir_RAFT_RefDesigns = os.path.join( dir_this,"ReferenceDesigns_TLP","RAFT")
+yaml_tlp_200m = os.path.join( dir_RAFT_RefDesigns,"NREL_15MW_TLP_200m.yaml")
+
 dir_04eg = os.path.dirname( dir_this )
 raft_04_umaine = os.path.join(dir_04eg, "outputs",
     "04_umaine_semi_raft_opt","raft_designs","raft_design_0.yaml")
 
 #%%
 # open the design YAML file and parse it into a dictionary for passing to raft
-with open( raft_04_umaine ) as file:
+with open( yaml_tlp_200m ) as file:
         design = yaml.load(file, Loader=yaml.FullLoader)
 #%%
 # Create the RAFT model (will set up all model objects based on the design dict)
