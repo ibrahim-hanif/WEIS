7042e2b1bb9b77be5777c298420a2e7767bfb006
ibrahim-hanif <guptavasudev37@gmail.com>
2026-06-10 13:16:16 +0200

    learning

--- examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/RAFT_eg_repo/examples/example_from_yaml.py
+++ examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/RAFT_eg_repo/examples/example_from_yaml.py
@@ -18,11 +18,11 @@ def run_example(plot_flag = False):
     # - static props: weight, mass, hydrostatics, and linearized mooring force and stiffness, about the system’s equilibrium position
     model.analyzeUnloaded()
 
-    # Compute natural frequencies
+    # Compute natural frequencies and mode shapes
     model.solveEigen()
 
-    # Simulate the different load cases
-    model.analyzeCases(display=1)
+    # Dynamic response simulation of the different load cases
+    model.analyzeCases(display=1) # (display metrics on)
 
     if plot_flag:
         # Plot the power spectral densities from the load cases
@@ -32,6 +32,8 @@ def run_example(plot_flag = False):
         model.plot(plot_frame=True) # flag plot_frame is used to plot the structural nodes and rigid links that are part of the structure. The default is False
 
         plt.show()
+
+    return model
 #%%
 if __name__ == "__main__":
     if len(sys.argv) == 2:
@@ -42,6 +44,14 @@ if __name__ == "__main__":
         print("Usage: python example_from_yaml.py <True/False>")
         print("  The last argument is an optional declaration to show or suppress the plots (default is True)")
         
-    run_example(plot_flag = plot_flag)
+    model = run_example(plot_flag = True) # plot_flag ; def above
         
+# %%
+# "resuls" dict: RAFT saves all its output data in a “results” dictionary that is a member of the Model class
+results = model.results
+for key,_ in results.items(): print(key)
+
+## PSDs of load cases stored as such, for the 1st turbine [0], 1st load case [0]
+# results['case_metrics'][0][0]
+
 # %%
