f63700c1557f44c6cfb3df0e32a49ade0985d48d
ibrahim-hanif <guptavasudev37@gmail.com>
2026-06-17 11:54:35 +0200

    learning

--- examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/RAFT_eg_repo/examples/example-WAMIT_Coefs.py
+++ examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/RAFT_eg_repo/examples/example-WAMIT_Coefs.py
@@ -1,19 +1,19 @@
+#%%
 # example script for running RAFT with second-order loads computed internally with the slender-body approximation based on Rainey's equation
-
 import numpy as np
 import matplotlib.pyplot as plt
 import yaml
 import raft
 import os
 import os.path as path
-
+#%%
 # open the design YAML file and parse it into a dictionary for passing to raft
 flNm = 'OC4semi-WAMIT_Coefs'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 flPath = path.join(current_dir, flNm + '.yaml')
 with open(flPath) as file:
     design = yaml.load(file, Loader=yaml.FullLoader)
-
+#%%
 # Create the RAFT model (will set up all model objects based on the design dict)
 model = raft.Model(design)
 
@@ -27,3 +27,4 @@ plt.show()
 
 # 0.02
 # 12.37
+# %%
