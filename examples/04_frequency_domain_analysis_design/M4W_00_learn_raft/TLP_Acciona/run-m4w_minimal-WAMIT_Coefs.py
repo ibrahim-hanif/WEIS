a8394feb9d60f05a97ff8f62de00f43a76d68b1f
ibrahim-hanif <guptavasudev37@gmail.com>
2026-06-22 14:31:29 +0200

    updated with workflow for diff analy results

--- examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/TLP_Acciona/run-m4w_minimal-WAMIT_Coefs.py
+++ examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/TLP_Acciona/run-m4w_minimal-WAMIT_Coefs.py
@@ -21,11 +21,68 @@ model = raft.Model(design)
 # Evaluate the system properties and equilibrium position before loads are applied
 model.analyzeUnloaded()
 
-model.analyzeCases(display=1)
-
-model.plotResponses()
-plt.show()
-
-# 0.02
-# 12.37
 # %%
+# "resuls" dict: RAFT saves all its output data in a “results” dictionary that is a member of the Model class
+results = model.results
+for key,_ in results.items(): print(key)
+
+## PSDs of load cases stored as such, for the 1st turbine [0], 1st load case [0]
+# results['case_metrics'][0][0]
+results["response"]
+
+#%%
+# 1. static properties
+
+# Important checks:
+# C33 > 0 (heave restoring)
+# C55 > 0 (pitch restoring)
+# CG below/near waterplane for stability
+
+fowt = model.fowtList[0]
+print("Mass:", fowt.M_struc[0,0])
+print("CG:", fowt.rCG)
+print("Hydrostatic stiffness:\n", fowt.C_hydro)
+
+# %%
+# 2. mean resp to steady loads
+model.analyzeCases(display=1) # TODO: or analyzeLoads()
+
+print(model.results['mean'])
+
+# This gives:
+
+# mean surge / sway / heave
+# mean pitch / roll
+# contributions from:
+
+# wind
+# current
+# wave drift (if included)
+
+#%%
+# 3. Compute natural frequencies and mode shapes
+model.solveEigen()
+
+print(model.results['eigen'])
+
+# If any mode:
+
+# is zero → missing stiffness
+# is very high → over-stiff proxy matrix
+
+#%%
+# 4. Dynamic response (RAOs / motions)
+model.solveDynamics()
+
+results = model.results['dynamic']
+
+# Typical contents:
+
+# RAOs (motion amplitude vs frequency)
+# platform motions (complex values)
+
+freq = results['freq']
+surge_RAO = results['RAO'][0]   # DOF 0 = surge
+pitch_RAO = results['RAO'][4]
+
+#%%
