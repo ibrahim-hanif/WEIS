3449b74e3f7b4323fabcfa2ae743e9a560578029
ibrahim-hanif <guptavasudev37@gmail.com>
2026-07-09 12:09:00 +0200

    good working model with verif csv, Alhamdolillah!

--- examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/TLP_Acciona/run-m4w_minimal-WAMIT_Coefs.py
+++ examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/TLP_Acciona/run-m4w_minimal-WAMIT_Coefs.py
@@ -8,12 +8,22 @@ import raft
 import os
 import os.path as path
 #%%
+# analysis flags
+flag_flex_tower = False # False for rigid tower
+if flag_flex_tower: str_case = 'raft_flex_tower'
+else: str_case = 'raft'
+
+flag_save_csv = False
+
+#%%
+# read YAML file into model
 # open the design YAML file and parse it into a dictionary for passing to raft
 flNm = 'M4W-base_case-TLPwamit-mooringGeo'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 flPath = path.join(current_dir, flNm + '.yaml')
 with open(flPath) as file:
     design = yaml.load(file, Loader=yaml.FullLoader)
+
 #%%
 # Create the RAFT model (will set up all model objects based on the design dict)
 model = raft.Model(design)
@@ -43,7 +53,7 @@ print("\n Coupled stiffness:\n", fowt.ms.getCoupledStiffness())
 T_moor = fowt.ms.getTensions()
 print(" \n Mooring tension @ fairleads (kN): \n", T_moor[:8]/1e3 )
 print(" \n Mooring tension @ anchors (kN): \n", T_moor[8:]/1e3 )
-
+pretension_fairleads_kN = T_moor[0]/1e3
 # How-to parse the results dict
 """
 # "resuls" dict: RAFT saves all its output data in a “results” dictionary that is a member of the Model class
@@ -60,8 +70,21 @@ results["response"]
 model.calcOutputs()
 
 results = model.results
+properties = results['properties']
 print("\n Model properties:")
-results['properties']
+
+# ---- parse for later
+platform_mass = properties['substructure mass']
+platform_cgz = properties['substructure CG'][2]
+platform_inertia_xyz_1e9 = np.array(([
+    properties['roll inertia at subCG'] / 1e9,
+    properties['pitch inertia at subCG'] / 1e9,
+    properties['yaw inertia at subCG'] / 1e9
+]))
+tower_mass_t = properties['tower mass'][0]/1e3
+vol_disp_water_m3 = properties['buoyancy (pgV)']/(1025*9.81)
+
+properties
 
 # %%
 # 2. Compute natural frequencies and mode shapes of the system
@@ -72,6 +95,8 @@ print("\n Eigen-solutions are: \n", model.results['eigen'])
 freq_eigen = model.results['eigen']['frequencies']
 modes_eigen = model.results['eigen']['modes']
 naturalPeriods = 1 / freq_eigen
+if flag_flex_tower: # sort naturalPeriods according to the indices [1,0,5,3,4,2]
+    naturalPeriods = naturalPeriods[[1,0,5,3,4,2]]
 print("\n Natural periods:\n", naturalPeriods)
 
 # If any mode:
@@ -128,5 +153,40 @@ model.plot(
 )
 # model.plot(plot_frame=True) # flag plot_frame is used to plot the structural nodes and rigid links that are part of the structure. The default is False
 plt.show()
+
+#%%[markdown]
+# ### Post-processing: save results to CSV for comparison with Acciona results
+# %%
+# save data in base_case_verification.csv
+import pandas as pd
+df = pd.read_csv('base_case_verification.csv')
+#%%
+# -- parse csv using row and column names
+df.loc[0, str_case] = platform_mass
+df.loc[1, str_case] = platform_cgz
+df.loc[2, str_case] = np.array2string(platform_inertia_xyz_1e9,separator=',')
+df.loc[3, str_case] = tower_mass_t
+df.loc[4, str_case] = vol_disp_water_m3
+df.loc[5, str_case] = pretension_fairleads_kN
+# -- natural periods
+df.loc[6, str_case] = naturalPeriods[0]
+df.loc[7, str_case] = naturalPeriods[1]
+df.loc[8, str_case] = naturalPeriods[2]
+df.loc[9, str_case] = naturalPeriods[3]
+df.loc[10, str_case] = naturalPeriods[4]
+df.loc[11, str_case] = naturalPeriods[5]
+# -- calc error between 'acciona' and 'raft' columns in a loop
+for i in range(len(df)):
+    Vacciona = df.loc[i, 'acciona']
+    Vraft = df.loc[i, str_case]
+    # convert string to array
+    if isinstance(Vacciona, str): Vacciona = eval(Vacciona)
+    if isinstance(Vraft, str): Vraft = eval(Vraft)
+    Vacciona = np.array(Vacciona)
+    Vraft = np.array(Vraft)
+    # calc error
+    df.loc[i, f'error_{str_case}'] = np.linalg.norm(Vacciona-Vraft)/np.linalg.norm(Vacciona) * 100
+# -- save df
+if flag_save_csv: df.to_csv('base_case_verification.csv', index=False)
 
 # %%
