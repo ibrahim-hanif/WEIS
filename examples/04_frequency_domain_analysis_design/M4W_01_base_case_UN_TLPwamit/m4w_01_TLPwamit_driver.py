813d76c4a0372133cb39d42af86f0f7640fa5b5d
ibrahim-hanif <guptavasudev37@gmail.com>
2026-07-14 15:04:11 +0200

    init base case weis model with TLPwamit, AL

---
+++ examples/04_frequency_domain_analysis_design/M4W_01_base_case_UN_TLPwamit/m4w_01_TLPwamit_driver.py
@@ -0,0 +1,43 @@
+#%%[markdown]
+# TODO:
+# 1. how to incl point_inertia (as in raft yaml), cf. omdao_raft.py, line 145
+
+#%%
+import os
+from weis import weis_main
+from wisdem.inputs.validation import load_yaml
+
+# TEST_RUN will reduce the number and duration of simulations
+TEST_RUN = True # TODO
+flag_GBO = False # TODO
+
+#%%
+## File management
+run_dir = os.path.dirname( os.path.abspath(__file__) )
+# -- geometry
+fname_wt_input = os.path.join(run_dir, "geometryOpt.yaml")
+# -- modelling
+fname_modeling_options = os.path.join(run_dir, "modelOpts.yaml")
+# -- analysis
+if flag_GBO:
+    fname_analysis_options = os.path.join(run_dir, "analysisOpt.yaml")
+else:
+    fname_analysis_options = os.path.join(run_dir, "analysisNOopt.yaml")
+
+#%%
+# run WEIS
+wt_opt, modeling_options, opt_options = weis_main(fname_wt_input, 
+                                                 fname_modeling_options, 
+                                                 fname_analysis_options,
+                                                 test_run=TEST_RUN
+                                                 )
+
+#%%
+# Test that the input we are providing RAFT has not changed
+this_raft_input = load_yaml(os.path.join(run_dir,"outputs","04_umaine_semi_raft_opt","raft_designs","raft_design_0.yaml"))
+standard_raft_input = load_yaml(os.path.join(run_dir, "..", "00_setup", "ref_turbines", "IEA-15-240-RWT_VolturnUS-S_raft.yaml"))
+# Disable this test because we get slightly different inputs on the linux CI
+assert(this_raft_input != standard_raft_input)
+
+# If the values have changed for a purpose, move this_raft_input to standard_raft_input and commit
+# %%
