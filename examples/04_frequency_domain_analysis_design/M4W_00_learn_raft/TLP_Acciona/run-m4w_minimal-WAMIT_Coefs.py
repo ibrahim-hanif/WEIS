2e10c7313faaf670c98b9d2f8ad888673530a659
ibrahim-hanif <guptavasudev37@gmail.com>
2026-07-22 11:29:07 +0200

    post-proces updates for ease

--- examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/TLP_Acciona/run-m4w_minimal-WAMIT_Coefs.py
+++ examples/04_frequency_domain_analysis_design/M4W_00_learn_raft/TLP_Acciona/run-m4w_minimal-WAMIT_Coefs.py
@@ -1,8 +1,18 @@
-#%%
-# example script for running RAFT with second-order loads computed internally with the slender-body approximation based on Rainey's equation
+#%%[markdown]
+# # Made4Wind base case FOWT RAFT model
+#
+# info: example script for running RAFT with second-order loads computed internally with the slender-body approximation based on Rainey's equation
 # (copy of oc4semi-WAMIT_Coefs.py)
+#
+# NOTE: Calibration process for the platform:
+# 1. define overall geometry of the platform and mooring, except platform ballast (l_fill)
+# 2. match CG: tune l_fill ballast
+# 3. match mass and inertia: copy difference from acciona's and use point_inertia 
+
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import yaml
 import raft
 import os
@@ -23,7 +33,12 @@ flag_flex_tower = False # False for rigid tower
 if flag_flex_tower: str_case = 'raft_flex_tower'
 else: str_case = 'raft'
 
+flag_m4w_wt = False
+
 flag_save_csv = False
+
+# saved data in base_case_verification.csv
+df = pd.read_csv('base_case_verification.csv')
 
 #%%
 # read YAML file into model
@@ -31,6 +46,10 @@ flag_save_csv = False
 flNm = 'M4W-base_case-TLPwamit-mooringGeo'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 flPath = path.join(current_dir, flNm + '.yaml')
+
+if flag_m4w_wt:
+    flPath = os.path.join(current_dir,"..","..","M4W_01_base_case_UN_TLPwamit","outputs","raft_designs","raft_design_0.yaml")
+
 with open(flPath) as file:
     design = yaml.load(file, Loader=yaml.FullLoader)
 
@@ -82,8 +101,10 @@ model.calcOutputs()
 results = model.results
 properties = results['properties']
 print("\n Model properties:")
-
-# ---- parse for later
+properties
+#%%
+# - parse model properties
+# ---- raft model
 platform_mass = properties['substructure mass']
 platform_cgz = properties['substructure CG'][2]
 platform_inertia_xyz_1e9 = np.array(([
@@ -94,7 +115,15 @@ platform_inertia_xyz_1e9 = np.array(([
 tower_mass_t = properties['tower mass'][0]/1e3
 vol_disp_water_m3 = properties['buoyancy (pgV)']/(1025*9.81)
 
-properties
+# ---- acciona
+acciona_platform_mass = eval(df.iloc[0]['acciona'])
+acciona_platform_cgz = eval(df.iloc[1]['acciona'])
+acciona_platform_inertia = np.array(eval(df.iloc[2]['acciona']))
+
+# ---- differences
+print(f" -1. platform cgz diff: {acciona_platform_cgz-platform_cgz}")
+print(f" -2. platform mass diff: {acciona_platform_mass-platform_mass}")
+print(f" -3. platform inertia diff (x 10^9): {acciona_platform_inertia-platform_inertia_xyz_1e9}")
 
 # %%
 # 2. Compute natural frequencies and mode shapes of the system
@@ -118,8 +147,6 @@ print("\n Natural periods:\n", naturalPeriods)
 # 3. mean resp to steady loads
 model.analyzeCases(display=1, RAO_plot=True) # TODO: or analyzeLoads()
 
-print(model.results['mean_offsets'])
-
 # This gives:
 
 # mean surge / sway / heave
@@ -129,6 +156,12 @@ print(model.results['mean_offsets'])
 # wind
 # current
 # wave drift (if included)
+
+# ---- check with SIMA outputs
+case_metrics_0 = model.results['case_metrics'][0][0]
+print(f" - Tmoor_max's max: { np.max(case_metrics_0['Tmoor_max']) }")
+print(f" - Tmoor_avg's mean: { np.mean(case_metrics_0['Tmoor_avg']) }")
+print(f" - Tmoor_std's mean: { np.mean(case_metrics_0['Tmoor_std']) }")
 
 #%%
 """
@@ -166,10 +199,7 @@ plt.show()
 
 #%%[markdown]
 # ### Post-processing: save results to CSV for comparison with Acciona results
-# %%
-# save data in base_case_verification.csv
-import pandas as pd
-df = pd.read_csv('base_case_verification.csv')
+
 #%%
 # -- parse csv using row and column names
 df.loc[0, str_case] = platform_mass
