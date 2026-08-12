c00eac1e010953416e11f80ca2440d20aa0a74e3
ibrahim-hanif <guptavasudev37@gmail.com>
2026-08-12 13:53:46 +0200

    final model: basecase DT-tower optim, AL

--- examples/04_frequency_domain_analysis_design/M4W_01b_basecase_UN_optimDTtower/m4w_01b_optimDTtower_driver.py
+++ examples/04_frequency_domain_analysis_design/M4W_01b_basecase_UN_optimDTtower/m4w_01b_optimDTtower_driver.py
@@ -78,14 +78,24 @@ print(" ", freq_floater[0:2], " Hz \n" )
 
 print("LSS desvars:")
 print(" ", wt_opt["drivese.L_h1"], wt_opt["drivese.L_12"], wt_opt["drivese.lss_diameter"], wt_opt["drivese.lss_wall_thickness"] )
-print(" ")
-print("--- constr_ max ---")
-print("- lss: ",
-      np.max(wt_opt["drivese.constr_lss_vonmises"])
-      )
-print("- bedplate: ",
-      np.max(wt_opt["drivese.constr_bedplate_vonmises"])
-      )
+print("Bedplate wall thickness:")
+print(" ", wt_opt["drivese.bedplate_wall_thickness"])
+
+print("\n--- constr_ max ---")
+print("- lss: ", np.max(wt_opt["drivese.constr_lss_vonmises"]) )
+print("- bedplate: ", np.max(wt_opt["drivese.constr_bedplate_vonmises"]) )
+print("- defl mb1: ", np.max(wt_opt["drivese.constr_mb1_defl"]) )
+print("- defl mb2: ", np.max(wt_opt["drivese.constr_mb2_defl"]) )
+
+print("- constr_shaft_deflection:", wt_opt["drivese.constr_shaft_deflection"])
+print("- constr_shaft_angle:", wt_opt["drivese.constr_shaft_angle"])
+print("- constr_stator_deflection:", wt_opt["drivese.constr_stator_deflection"])
+print("- constr_stator_angle:", wt_opt["drivese.constr_stator_angle"])
+print("- constr_hub_diameter:", wt_opt["drivese.constr_hub_diameter"])
+print("- constr_length:", wt_opt["drivese.constr_length"])
+print("- constr_height:", wt_opt["drivese.constr_height"])
+print("- constr_access:", wt_opt["drivese.constr_access"])
+print("- constr_ecc:", wt_opt["drivese.constr_ecc"])
 
 print("\nTower-top / drivetrain bedplate base loads:")
 print(" - base_F: ", wt_opt['drivese.base_F'])
@@ -100,5 +110,15 @@ print(f"tower mass: {wt_opt["towerse.tower_mass"]}")
 print("\n--- RNA properties ---")
 print(f"RNA mass: {wt_opt["drivese.rna_mass"]}")
 print(f"RNA cm: {wt_opt["drivese.rna_cm"]}")
+
+print("\n--- Dynamic properties (RAFT) ---")
+print(f" Max_Offset [m]: {wt_opt["raft.Max_Offset"]}")
+print(f" Heave_avg [m]: {wt_opt["raft.heave_avg"]}")
+print(f" Max_PtfmPitch [deg]: {wt_opt["raft.Max_PtfmPitch"]}")
+print(f" Max_nac_accel [m/s^2]: {wt_opt["raft.max_nac_accel"]}")
+print(f" Max_tower_base [10^9 Nm]: {wt_opt["raft.max_tower_base"]/1e9}")
+
+print("\n--- Merit figures ---")
+print(f" LCOE [USD/kW/h]: {wt_opt["financese.lcoe"]}")
 # -----------------------------------------------------------------------
 # %%
